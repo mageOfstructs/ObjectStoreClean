514273d97d96cf6da4886f985be5f6f73ca7c405
mageOfStructs <jason.puschnig@gmx.at>
2025-03-31 12:03:32 +0200

    DriverManager demo

--- src/main/java/org/example/test/Database.java
+++ src/main/java/org/example/test/Database.java
@@ -1,4 +1,17 @@
 package org.example.test;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Database {
+    private static final String DEFAULT_URL = "jdbc:oracle:thin:@tcif.htl-villach.at:1521/orcl";
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        String driver = "oracle.jdbc.OracleDriver";
+        Class.forName(driver);
+        System.out.println("Driver loaded");
+        Connection c = DriverManager.getConnection(DEFAULT_URL, "d3b13", "d3b13");
+        System.out.println("Connected");
+        c.close();
+    }
 }
